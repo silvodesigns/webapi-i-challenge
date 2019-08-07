483ffc610be0cb574d529f58d84cc273ad7133a4
Kevin-S <kevsilvo@gmail.com>
2019-08-07 19:25:41 -0400

    get/users:id endpoint

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const db = require('./data/db.js');
 //lets create the server
 const server = express();
 
+//middleware
+server.use(express.json());
+
 server.get('/',(req,res) => {
     res.send('<h1>Welcome to the Project Database</h1>')
 });
@@ -26,6 +29,42 @@ server.get('/users',(req,res)=>{
     db.find()
     .then(users => {
         res.json(users)
+    })
+    .catch(err =>{
+        res.status(500).json({
+            err:err,
+            message: "The users information could not be retrieved"
+        })
+    })
+    
+});
+
+// When the client makes a GET request to /api/users/:id:
+
+// If the user with the specified id is not found:
+
+// ----------return HTTP status code 404 (Not Found).
+// ----------return the following JSON object: 
+// ----------{ message: "The user with the specified ID does not exist." }.
+
+// If there's an error in retrieving the user from the database:
+// ---------- cancel the request.
+// ----------respond with HTTP status code 500.
+
+server.get('/users/:id',(req,res)=>{
+
+    const { id } = req.params;
+
+    db.findById(id)
+    .then(users => {
+        if(id){
+            res.json(users)
+        } else {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        }
+       
     })
     .catch(err =>{
         res.status(500).json({
