4bcf7383d650861757041202815a268cdc707f0c
Kevin-S <kevsilvo@gmail.com>
2019-08-07 19:12:21 -0400

    added first server.get

--- index.js
+++ index.js
@@ -4,4 +4,24 @@
 const express = require('express');
 
 //lets specify the database file we working with
-const db=require('./data/db.js');
+const db = require('./data/db.js');
+
+//lets create the server
+const server = express();
+
+server.get('/',(req,res) => {
+    res.send('<h1>Welcome to the Project Database</h1>')
+})
+
+//When the client makes a GET request to /api/users:
+
+// If there's an error in retrieving the users from the database:
+// ---------cancel the request.
+// ---------respond with HTTP status code 500.
+// ---------return the following JSON object: 
+// ---------{ error: "The users information could not be retrieved." }.
+
+//Let's make it listen
+server.listen(4000,()=>{
+    console.log("server is running on port 4000");
+})
