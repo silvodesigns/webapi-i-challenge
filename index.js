a3116a353dec50693b6a314e327d84f5884e2f4c
Kevin-S <kevsilvo@gmail.com>
2019-08-07 19:06:01 -0400

    added dependencies

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // implement your API here
+
+//let's import the express library into our project
+const express = require('express');
+
+//lets specify the database file we working with
+const db=require('./data/db.js');
