7e4d85e45017276d8376a140a3c9a9d52027a34d
Kevin-S <kevsilvo@gmail.com>
2019-08-07 19:56:02 -0400

    delete/users endpoint

--- index.js
+++ index.js
@@ -121,6 +121,42 @@ server.post('/users',(req,res) => {
 });
 
 
+// When the client makes a DELETE request to /api/users/:id:
+
+// If the user with the specified id is not found:
+
+// -----return HTTP status code 404 (Not Found).
+// -----return the following JSON object: { message: "The user with the specified ID does not exist." }.
+
+// If there's an error in removing the user from the database:
+// -----cancel the request.
+// -----respond with HTTP status code 500.
+// -----return the following JSON object: { error: "The user could not be removed" }.
+
+server.delete('/users/:id',(req,res) => {
+
+
+   const { id } = req.params;
+ 
+    db.remove(id)
+     .then(deletedPerson => {
+      if(deletedPerson){
+         res.json(deletedPerson);
+      } else {
+          res.status(404).json({
+              message: "The user with the specified ID does not exist."
+          })
+      }
+     })
+     .catch(err => {
+         res.status(500).json({
+             err: err,
+             message: 'The user could not be removed'
+         })
+     })
+ });
+ 
+
 //Let's make it listen
 server.listen(4000,()=>{
     console.log("server is running on port 4000");
